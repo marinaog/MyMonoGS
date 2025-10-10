24f5d106ce180b3d9c4674bd84c8786fe3bea0d1
marinaog <marinaorozcglez@gmail.com>
2025-10-10 14:46:30 +0200

    cleaned version

--- post_evaluation.py
+++ post_evaluation.py
@@ -2,8 +2,6 @@
 This file was created by me to perform evaluation after SLAM, using the saved point cloud and estimated poses.
 It uses both, raw and sRGB estimated poses, in order to be able to compare both resulting renders from the same camera poses.
 """""
-
-
 import torch
 from munch import munchify
 
@@ -18,14 +16,15 @@ import json
 import numpy as np
 from tqdm import tqdm
 
+# Settings
 both = True
 raw = True
 scene = "candles"
 runnum = "short"
 
+# Paths
 pathresults = f"results/datasets_rawslam/"+scene+runnum
-# pathraw = f"results/datasets_rawslam/"+scene+"short_raw"
-# pathsrgb = f"results/datasets_rawslam/"+scene+"short_srgb"
+
 if raw:
     scene_name = scene + "_raw"
     ply_path = pathresults + f"_raw/point_cloud/final/point_cloud.ply"
@@ -37,11 +36,11 @@ else:
     config_path = f"configs/rgbd/rawslam/{scene}.yaml"
     est_pose_path = pathresults + f"_srgb/plot/trj_final.json"
 
-# est_pose_path = path + f"/plot/trj_final.json"
 data_type = "rawslam"
 save_dir = f"test{data_type}_{scene_name}_{runnum}"
 
 
+# Load config and dataset
 config = load_config(config_path)
 config["Dataset"]["type"] = data_type
 pipeline_params = munchify(config["pipeline_params"])
@@ -51,20 +50,15 @@ background = torch.tensor([0, 0, 0], dtype=torch.float32, device="cuda")
 
 print('len(dataset)',len(dataset))
 
-
 opt_params = munchify(config["opt_params"])
 gaussians = GaussianModel(model_params.sh_degree, config=config)
-# gaussians.training_setup(opt_params)
-
 gaussians.load_ply(ply_path)
 projection_matrix = getProjectionMatrix2(znear=0.01, zfar=100.0, fx=dataset.fx, fy=dataset.fy,
                                          cx=dataset.cx, cy=dataset.cy, W=dataset.width, H=dataset.height).transpose(0, 1)
 projection_matrix = projection_matrix.to(device="cuda")
 
+# Load estimated poses
 cameras = {}
-
-# kf_indices = list(range(0, len(dataset), 5))
-kf_indices = []
 with open(est_pose_path, 'r') as f:
     data = json.load(f)
 kf_indices = data['trj_id']
@@ -78,6 +72,7 @@ for i in tqdm(kf_indices):
     viewpoint.compute_grad_mask(config)
     cameras[i] = viewpoint
 
+# Evaluate
 rendering_result = eval_rendering(
                 cameras,
                 gaussians,
